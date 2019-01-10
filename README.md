603f0af5434bdaa3fb2ec8a4a976ad9fd3eb98e4
Joe <sheninter@gmail.com>
2019-01-10 09:50:21 +0800

    生成qr code。

--- README.md
+++
@@ -1,2 +0,0 @@
-# urlQRCode
-firefox extension
---
+++ popup/popup.js
@@ -0,0 +1,15 @@
+
+function generateQR(tab) {
+  var url = tab.url
+  new QRCode(document.getElementById("qrcode"), {
+    text: url,
+    width: 80,
+    height: 80
+  });
+}
+
+browser.tabs.query({
+  active: true
+}).then( (tabs) => {
+  generateQR(tabs[0])
+})
